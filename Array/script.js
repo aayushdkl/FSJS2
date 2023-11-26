10ce697ee1b8b27e33f141b8da8630779693b088
aayushdkl <079bei001.aayush@sagarmatha.edu.np>
2023-11-26 21:53:44 +0545

    Some properties of array

---
+++ Array/script.js
@@ -0,0 +1,33 @@
+//Arrays in depth and for of loop
+
+// let names = ['aayush','john','eminem','taylor','bipul'];
+// console.log(names);
+
+// //Properties of array.
+// //1. Length of array
+// console.log(names.length);
+
+// //2.
+// console.log(names[3]);
+
+// //3. last value of array when you dont know the length
+// console.log(names[names.length - 1]);
+
+// names[4] = "vinay";
+// console.log(names);
+
+
+//another way to declare array but not recommended
+// let names = new Array ("Name 1","Name 2"); 
+// console.log(names);
+
+
+
+
+
+
+
+
+
+
+
