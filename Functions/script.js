d3811f8902f04d01c848be336b71d7228ddb75bd
aayushdkl <079bei001.aayush@sagarmatha.edu.np>
2023-12-01 11:32:11 +0545

    First file added

---
+++ Functions/script.js
@@ -0,0 +1,15 @@
+//Topic : Functions
+// function sum(value1,value2) //function definition
+// {
+//     let num1=18;
+//     let num2 = 25;
+
+//     console.log('Addition of two numbers is',value1+value2);
+// }
+
+// sum(52,61); // calling of function
+// sum(10,11);
+// sum(30,67);
+
+//Normal function and function with parameters
+
