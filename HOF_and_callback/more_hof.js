15ab004c7a957ba000cd89e2a32964885c28d17f
aayushdkl <079bei001.aayush@sagarmatha.edu.np>
2024-01-07 21:08:16 +0545

    some modifications

--- HOF_and_callback/more_hof.js
+++ HOF_and_callback/more_hof.js
@@ -14,15 +14,72 @@
 // console.log(sumone(...vari)); //Spread
 
 // Rest
-function sum(...args)  //Rest of data
-{
-    console.log(args);
-    let sum = 0;
-    for(const x of args)
-    {
-        sum = sum + x;
-    }
-    return sum;
-}
+// function sum(...args)  //Rest of data
+// {
+//     console.log(args);
+//     let sum = 0;
+//     for(const x of args)
+//     {
+//         sum = sum + x;
+//     }
+//     return sum;
+// }
 
-console.log(sum(1,2,3,4,5,6,7,8,9,0));
+// console.log(sum(1,2,3,4,5,6,7,8,9,0));
+
+// Try and Catch
+
+//Syntax
+// try{
+//     let first_name = "aayush";
+//     console.log(first_name + "" + lastname);
+// }
+
+// catch(err)
+// {
+//     console.log("Variable name missing");
+// }
+
+// finally
+// {
+//     console.log("I will run");
+// }
+
+
+// throw => Explore this topic
+
+
+//  Different types of error we have in JavaScipt
+
+/* 
+    - Reference error
+    - Syntax error
+    - Type error
+*/
+
+//  promise
+//  - pending
+//  - fulfilled
+//  - rejected
+
+//  Event Loop
+
+const userOne = () => 
+{
+    console.log("hello one");
+};
+const userTwo = () => 
+{
+    console.log("hello two");
+    setTimeout(() => {
+        console.log("hello i am inside");
+    }, 3000);
+};
+const userthree = () => 
+{
+    console.log("hello three");
+};
+
+userOne();
+userTwo();
+userthree();
