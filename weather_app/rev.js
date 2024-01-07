547ede28b5222dfe236da270677c26676d47e241
aayushdkl <079bei001.aayush@sagarmatha.edu.np>
2024-01-07 21:27:33 +0545

    weather app folder

---
+++ weather_app/rev.js
@@ -0,0 +1,25 @@
+//Map, filter and reduce
+// primise and fetch api
+//weather app
+
+//map => interate over array and perform an action on each item
+//filter => iterate over array and filter out the element
+//reduce => single value
+
+
+//map
+// const numbers = [1,2,3,4,5,6,7,8,9,10];
+// const res = numbers.map((val)=>val * val);
+// console.log(res);
+
+//filter
+// const names = ["ayush","suresh","naresh","umesh","prabesh","rupesh","rajesh","dinesh"];
+// const res = names.filter((val)=> val.includes("esh"));
+// console.log(res);
+
+//reduce
+// const numbers = [1,2,3,4,5,6,7,8,9];
+// const res = numbers.reduce((acc,num)=>acc + num,5);
+// console.log(res);
+
+
