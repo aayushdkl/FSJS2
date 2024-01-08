09f5ce9988459fd82258654b31668b28b3b04f73
aayushdkl <079bei001.aayush@sagarmatha.edu.np>
2024-01-08 20:30:46 +0545

    weatherapp jscode

---
+++ weather_app/script.js
@@ -0,0 +1,15 @@
+let data; 
+
+const inputBox = document.getElementById("inputBox");
+const countryName = document.getElementById("countryName");
+const stateName = document.getElementById("stateName");
+const cityName = document.getElementById("cityName");
+const humidity = document.getElementById("humidity");
+const windSpeed = document.getElementById("windSpeed");
+const temperature = document.getElementById("temperature");
+const logoImage = document.getElementById("logoImage");
+const weatherStatus = document.getElementById("weatherStatus");
+
+const getData = (event) => {
+    event.preventDefault();
+}
